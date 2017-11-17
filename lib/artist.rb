50a1fc573eee5485caf5d4e466acca235400a009
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-11-17 18:42:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,14 +11,15 @@
 
 class Artist
   attr_accessor :name, :songs
+
   def initialize(name)
     @name = name
-
-  @songs = []
+    @songs = []
+    
 end
 
 def initialize(song)
-  @song << song
+  @songs << song
   song.artist = self
 end
 
