9e59a5eec099e78c76c42d8a6522c3f42efed896
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-11-17 18:14:51 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,9 @@
+#The Doctor model:
+#A doctor should be initialized with a name and an empty @appointments array.
+#The Doctor class needs an instance method, #add_appointment, that takes in an instance of the Appointment class and adds that appointment to the doctor's @appointments array. The method should also tell that appointment that it belongs to that doctor.
+#The Doctor class needs an instance method, #appointments, that returns the @appointments array.
+#The Doctor class needs an instance method, #patients, that iterates over that doctor's appointments and collects the patient that belongs to each appointment.
+
 class Doctor
   attr_accessor :name, :appointments
   def initialize(name)
@@ -8,10 +14,9 @@ class Doctor
     def add_appointment(app)
       @appointments << self
       app.doctor = self
-    end 
+    end
 
-    def patients 
+    def patients
     self.appointments.collect{ |app| app.patient }
    end
    end
-
